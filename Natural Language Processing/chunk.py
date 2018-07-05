c9391a97f5da93616989da55f5c731c2a958e933
Ajay Kumar Singh <ajaykumar.kumar900@gmail.com>
2018-07-05 11:30:36 +0530

    Text Chunks

---
+++ Natural Language Processing/chunk.py
@@ -0,0 +1,29 @@
+import numpy as np
+from nltk.corpus import brown
+import nltk
+
+nltk.download('brown')
+
+
+def chunker(input_data, N):
+    input_words = input_data.split(' ')
+    output = []
+    cur_chunk = []
+    count = 0
+    for word in input_words:
+        cur_chunk.append(word)
+        count+=1
+        if count == N:
+            output.append(' '.join(cur_chunk))
+            count, cur_chunk = 0, []
+
+    output.append(' '.join(cur_chunk))
+    return output
+
+if __name__ == '__main__':
+    input_data = ' '.join(brown.words()[:12000])
+    chunk_size = 700
+    chunks = chunker(input_data, chunk_size)
+    print('\nNumber of text chunks = ',len(chunks),'\n')
+    for i,chunk in enumerate(chunks):
+        print('Chunk', i+1,'==>', chunk[:50])
